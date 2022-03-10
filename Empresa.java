762e18e962fbcfcaa1da5c7114bf2d73f3de93f1
JOAORICARDO7 <83128494+JoaoricardodeOA@users.noreply.github.com>
2022-03-10 14:47:12 +0000

    Initial commit

---
+++ Empresa.java
@@ -0,0 +1,33 @@
+public class Empresa{
+  private String endereco;
+  private String cidade;
+  private String telefone;
+  private String email;
+  private boolean matriz;
+  private Funcionario gerente;
+  Empresa(String endereco, String cidade, String telefone, String email, boolean matriz){
+    this.endereco = endereco;
+    this.cidade = cidade;
+    this.telefone = telefone; 
+    this.email = email;
+    this.matriz = matriz;
+  }
+  void setEndereco(String endereco){
+    this.endereco = endereco;
+  }
+  String getEndereco(){
+    return endereco;
+  }
+  void setMatriz(boolean matriz){
+    this.matriz = matriz;
+  }
+  boolean isMatriz(){
+    return matriz;
+  }
+  Funcionario getGerente(){
+    return gerente;
+  }
+  void setGerente(Funcionario gerente){
+    this.gerente = gerente;
+  }
+}
---
+++ Funcionario.java
@@ -0,0 +1,12 @@
+public class Funcionario{
+  private String nome; 
+  private String cpf;
+  private String dataDeAniversario;
+  private String cargo;
+  Funcionario(String nome, String cpf, String dataDeAniversario, String cargo){
+    this.nome = nome;
+    this.cpf = cpf;
+    this.dataDeAniversario = dataDeAniversario;
+    this.cargo = cargo;
+  }
+}
